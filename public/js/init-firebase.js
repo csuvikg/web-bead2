9b51d95ebbb75a11b2229e670bdf3c4aab749ec0
csuvikg <1455667Git>
2019-04-29 17:22:18 +0200

    Init project

---
+++ public/js/init-firebase.js
@@ -0,0 +1,8 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyDdydSSTdHQE-uFyG4dwC6vtPAY88yAgu4",
+    authDomain: "pszi-info.firebaseapp.com",
+    databaseURL: "https://pszi-info.firebaseio.com",
+    projectId: "pszi-info",
+    storageBucket: "pszi-info.appspot.com",
+    messagingSenderId: "209820198501"
+});
