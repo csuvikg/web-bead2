c6b2bb3d3721252ad8a78d05b4a6342f9936d269
csuvikg <1455667Git>
2019-05-01 00:46:46 +0200

    Add elements to main page

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,9 @@
-
+// Initialize and add the map
+function initMap() {
+    const budapest = {lat: 47.497913, lng: 19.040236};
+    const map = new google.maps.Map(
+        document.getElementById('map'),
+        {zoom: 10, center: budapest}
+    );
+    const marker = new google.maps.Marker({position: budapest, map: map});
+}
