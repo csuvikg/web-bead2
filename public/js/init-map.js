bc6f74415f4afe1244f47e99704f43e656b2a0ee
csuvikg <1455667Git>
2019-05-01 21:32:04 +0200

    Add static data and results to main page

---
+++ public/js/init-map.js
@@ -0,0 +1,22 @@
+function initMap() {
+    const budapest = {lat: 47.497913, lng: 19.040236};
+    initMapWithPosition(budapest);
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            initMapWithPosition({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            });
+        });
+    }
+
+}
+
+function initMapWithPosition(position) {
+    const map = new google.maps.Map(
+        document.getElementById('map'),
+        {zoom: 10, center: position}
+    );
+    const marker = new google.maps.Marker({position, map});
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,32 @@
-// Initialize and add the map
-function initMap() {
-    const budapest = {lat: 47.497913, lng: 19.040236};
-    const map = new google.maps.Map(
-        document.getElementById('map'),
-        {zoom: 10, center: budapest}
-    );
-    const marker = new google.maps.Marker({position: budapest, map: map});
+import {data} from './data.js'
+
+function loadResults() {
+    const container = document.getElementById("results");
+    container.innerHTML = "";
+
+    for (let [index, item] of data.entries()) {
+        container.innerHTML += `<div class="card mb-3">
+  <h5 class="card-header" style="cursor: pointer" id="resultHeader${index}" data-toggle="collapse" data-target="#result${index}" aria-expanded="false" aria-controls="result${index}">
+    ${item.name} (${item.city})
+  </h5>
+  <div id="result${index}" class="collapse" aria-labelledby="resultHeader${index}" data-parent="#results">
+      <div class="card-body">
+        <h5 class="card-title">${item.zipCode} ${item.city}, ${item.address}</h5>
+        <h6 class="card-subtitle text-muted">${item.phoneNumber || "Nincs megadva telefonszám"}</h6>
+      </div>
+      <ul class="list-group list-group-flush">
+        ${item.officeHours != null ? "<li class='list-group-item'>Ügyeleti idő: " + item.officeHours + "</li>" : ""}
+        ${item.targetGroup != null ? "<li class='list-group-item'>Célcsoport: " + item.targetGroup + "</li>" : ""}
+        ${item.referral != null ? "<li class='list-group-item'>Beutaló/előjegyzés: " + item.referral + "</li>" : ""}
+        ${item.waitingList != null ? "<li class='list-group-item'>Átlagos várakozás: " + item.waitingList + "</li>" : ""}
+      </ul>
+      <div class="card-body">
+        <a href="${item.website}" target="_blank" rel="noopener noreferrer" class="card-link">Honlap</a>
+        <a href="#" target="_blank" rel="noopener noreferrer" class="card-link">Térkép</a>
+      </div>
+  </div>
+</div>`;
+    }
 }
+
+loadResults();
